977139ab40ad85709f904e7153bb65ede74284f1
Ranjith <kumar.srec45@gmail.com>
2015-09-10 16:42:38 +0530

    Create Image

--- services/openstack/Glance.py
+++ services/openstack/Glance.py
@@ -14,8 +14,8 @@ class Glance:
         imageFormat=image.split(".")[-1:][0]
         imageName = image.replace("."+imageFormat, "")
         print(imageName)
-
         response = Glance.rest.doGet(self.url+"/v2/images",self.data,self.headers)
+
         images = response["images"]
         status = True
         for image in images:
@@ -28,4 +28,7 @@ class Glance:
         return status
 
     def upload_img(self):
+        data = {"name": "testImg","container_format": "bare","disk_format": "qcow2","min_disk": 0,"min_ram": 0}
+        response = Glance.rest.doPost(self.url+"/v2/images",data,self.headers)
+        print(response["file"])
         print("upload_img")
